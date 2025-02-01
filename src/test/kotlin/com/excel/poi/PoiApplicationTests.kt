ddbef6e6ca15c40485e20510a5f42d30d85fb587
ironjin <cheoljin1234@naver.com>
2025-02-01 23:57:28 +0900

    Added: 기본 파일 추가

---
+++ src/test/kotlin/com/excel/poi/PoiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.excel.poi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PoiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
